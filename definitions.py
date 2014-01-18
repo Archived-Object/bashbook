5512cb887621e9e8cb42e84946a601fdc355396e
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2014-01-18 06:40:32 -0500

    whoops again

---
+++ definitions.py
@@ -0,0 +1,19 @@
+
+class User():
+	def __init__(self, uid, name, age, location, websites, interests, friends):
+		(	self.uid, self.name, self.age, self.location,
+			self.websites, self.interests, self.friends) = (
+				uid, name, age, location, websites, interests, friends
+			)
+
+class Post():
+	def __init__(self, post_id, creator_uid, text_content, meta_content, comment_ids):
+		(	self.post_id, self.creator_uid, self.text_content,
+			self.meta_content, self.comment_ids) = (
+				post_id, creator_uid, text_content,
+				meta_content, comment_ids)
+
+class Comment():
+	def __init__(self, comment_id, creator_uid, text_content):
+		(self.comment_id, self.creator_uid, self.text_content) = (
+			comment_id, creator_uid, text_content)
