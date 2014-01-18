673675ee87b06239ffc3fb3b844e86f10235b600
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2014-01-18 06:40:19 -0500

    shodcod

--- frontend.py
+++ frontend.py
@@ -1,6 +1,7 @@
 import curses, curses.textpad
 import time, math
 import serverio
+from definitions import *
 import textwrap
 
 #window object, global because curses
@@ -9,55 +10,21 @@ logged_in_user = None
 
 #these are dictionaries of data that would normally be populated by server
 #because this is GUI, putting it here.
+online_friends = {}
 users = {}
 posts = {}
 comments = {}
 
-class User():
-	def __init__(self, uid, name, age, location, websites, interests, friends):
-		(	self.uid, self.name, self.age, self.location,
-			self.websites, self.interests, self.friends) = (
-				uid, name, age, location, websites, interests, friends
-			)
+scroll_y=0
+chat_y=0
 
-	def add_friend(self):
-		curses.addstr(window.getmaxyx[0]-25, getmaxyx[1] ,"sending friend request.. ")
-		window.refresh()
-		if(self != logged_in_user and serverio.add_friend(self, logged_in_user) ):
-			curses.addstr(window.getmaxyx[0]-25, getmaxyx[1] ,"friend request sent     ")
-		else:
-			curses.addstr(window.getmaxyx[0]-25, getmaxyx[1] ,"friend request failed to send")
-		window.refresh()
 
-class Post():
-	def __init__(self, post_id, creator_uid, text_content, meta_content, comment_ids):
-		(	self.post_id, self.creator_uid, self.text_content,
-			self.meta_content, self.comment_ids) = (
-				post_id, creator_uid, text_content,
-				meta_content, comment_ids)
-
-	def post_comment(self, comment_text):
-		curses.addstr(window.getmaxyx[0]-25, getmaxyx[1] ,"posting comment...")
-		window.refresh()
-		if serverio.post_comment(self, logged_in_user, comment_text):
-			curses.addstr(window.getmaxyx()[0]-25, getmaxyx[1] ,"Post successful     ")
-		else:
-			curses.addstr(window.getmaxyx()[0]-25, getmaxyx[1] ,"Post failed       ")
-		window.refresh()
-
-	def get_username(self):
-		return users[self.creator_uid].name
-
-class Comment():
-	def __init__(self, comment_id, creator_uid, text_content):
-		(self.comment_id, self.creator_uid, self.text_content) = (
-			comment_id, creator_uid, text_content)
-
-	def get_username(self):
-		return users[self.creator_uid].name
+def get_username(post):
+	return users[post.creator_uid].name
 
 
 def startup():
+	curses.noecho()
 	curses.curs_set(0)
 	window.border(0)
 	window.addstr(12, 25, "Pretending to connect...")
@@ -67,86 +34,117 @@ def startup():
 	#time.sleep(0.5)
 	#window.getch()
 
-	curses.curs_set(1)
+	#curses.curs_set(1)
 
 def login():
 	global logged_in_user, users
 	logged_in_user = User(0,"Max",18,"Toronto",["maxwell.huang-hobbs.com"],["FUCKING NOTHING"],[])
 	users[0] = logged_in_user
-	users[1] = User(0,"Will",19,"Boston",["williamsaulnier.com"],[],[])
+	for friend in serverio.get_friends(logged_in_user):
+		users[friend.uid] = friend
 
+	update_posts(0)
 
-def fake_load():
-	global posts, users, comments
-	posts[0] = Post(0,0,"Just ate a sandwich, LOL0 testing the line wrapping also so this text should be enough to cause wrap, but I don't have an elegant fallback for wrapping", "feeling full", [0])
-	posts[1] = Post(1,1,"Just ate a sandwich, LOL1", "feeling full", [1,2,3])
+def update_posts(fish_depth):
+	for post in serverio.get_recent_posts(logged_in_user, fish_depth):
+		if(post.post_id not in posts.keys()):
+			posts[post.post_id] = post
 
-	comments[0] = Comment(0, 1, "dumb0")
-	comments[1] = Comment(1, 1, "dumb1")
-	comments[2] = Comment(2, 1, "dumb2")
-	comments[3] = Comment(3, 1, "dumb3")
+			for cid in post.comment_ids:
+				if not cid in comments.keys():
+					comments[cid] = serverio.get_comment_by_id(logged_in_user, cid)
+
 
 def printblock(x, y, width, header, body):
-	by = y+len(textwrap.wrap(body, width))+3;
+	disp_width = width - 5
+	by = y+len(textwrap.wrap(body, disp_width))+3;
 
-	if( y>=0 and by< window.getmaxyx()[0]):
+	if( y>=0 and by< window.getmaxyx()[0]-1):
 		curses.textpad.rectangle(window, y, x, by, x + width)
 		window.addstr(y+1, x+2, header)
 
 		#prints the body of the text post
-		disp_width = width - 5
 		wtext = textwrap.wrap(body, disp_width)
 		for line in range(len(wtext)):
 			window.addstr(y+2+line, x+5, wtext[line])
 	return by
 
-def redraw(scrolly):
+
+def redraw_both( return_chat):
+	window.clear()
+	window.refresh()
+	chat = redraw_chat(chat_y)
+	scroll = redraw_scroll(scroll_y)
+	return chat if return_chat else scroll
+
+def redraw_scroll(scrolly):
 	#redraw scroll
 	post_x  = 3
-	post_width = window.getmaxyx()[1]- 1 -post_x
+	post_width = window.getmaxyx()[1]-33
 	count_y=0
 	for i in range(len(posts)):
 		post = posts[i]
 
 		count_y = printblock(
-			post_x, count_y,post_width,
-			post.get_username(),
-			post.text_content) +1
+			post_x, count_y-scrolly ,post_width,
+			get_username( post),
+			post.text_content) +1 +scrolly
 		for c in range(len(post.comment_ids)):
 			count_y = printblock(
-					post_x+5, count_y, post_width-5,
-					comments[post.comment_ids[c]].get_username(),
-					comments[post.comment_ids[c]].text_content) +1
+					post_x+5, count_y-scrolly, post_width-5,
+					get_username( comments[post.comment_ids[c]] ),
+					comments[post.comment_ids[c]].text_content) +1 +scrolly
 
 	window.refresh()
+	return count_y
 
 def main_interface():
-	window.clear()
-	window.refresh()
-	scrolly = 0
-	redraw(scrolly)
+	redraw_both(True)
+	while(True):
+		scroll_interface()
+		chat_interface()
+		control_interface()
+
+def redraw_chat(chaty):
+	curses.textpad.rectangle(window, 0, window.getmaxyx()[1]-28, 2, window.getmaxyx()[1]-1)
+	window.addstr(1, window.getmaxyx()[1]-27, "Online Friends")
+
+	curses.textpad.rectangle(window, 3, window.getmaxyx()[1]-28, window.getmaxyx()[0]-2, window.getmaxyx()[1]-1)
+
+def scroll_interface():
+	global scroll_y
+	maxheight = redraw_both(False)
 	while True:
-		s = window.getch()
-		print(s)
-		if (s == curses.KEY_H):
-			scrolly += 1
-			redraw(scrolly)
-		if (s == curses.KEY_UP):
-			scrolly -= 1
-			redraw(scrolly)
-		if (s == curses.KEY_EXIT):
-			break
+		s = window.getkey()
+		if (s == "k" and scroll_y<=maxheight-window.getmaxyx()[0]):
+			scroll_y += 1
+			maxheight = redraw_both(False)
+		if (s == "j" and scroll_y>0):
+			scroll_y -= 1
+			maxheight = redraw_both(False)
 
-
-
+def chat_interface():
+	global chat_y
+	window.clear()
+	curses.textpad.rectangle(window, y, x, by, x + width)
+	window.refresh()
+	maxheight = redraw_both(True)
+	while True:
+		s = window.getkey()
+		if (s == "k" and chat_y<=maxheight-window.getmaxyx()[0]):
+			chat_y += 1
+			maxheight = redraw_both(True)
+		if (s == "j" and chat_y>0):
+			chat_y -= 1
+			maxheight = redraw_both(True)
 
 
 if __name__ == "__main__":
 	window = curses.initscr()
 
 	startup()
-	fake_load()
 	login()
+
 	main_interface()
 
 	curses.endwin()
